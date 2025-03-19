81a29d2f9bdb0f36a00f8b88afb3bbcac8af8628
Bohdan Masliannykov <bmasliannykov@MacBookAir.play.pl>
2025-03-19 19:23:54 +0100

    feat: setup local mongoose db

--- server.js
+++ server.js
@@ -1,9 +1,20 @@
 import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+
+dotenv.config();
 
 const app = express();
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-});
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    app.listen(PORT, async () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.error("Error connecting to MongoDB:", error);
+  });
